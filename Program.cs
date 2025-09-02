6808cfdca0e924873df26d96d2185e07d13885b1
Mena Erian <mena3koko2karas1@gmail.com>
2025-09-02 13:43:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection = new SqlConnection(
+                "Server=.;Database=ITIA;integrated security=true;trustservercertificate=true;"
+                );
+            SqlCommand sqlCommand = new SqlCommand("select * from Department;", sqlConnection);
+            
+            sqlCommand.CommandType = System.Data.CommandType.Text;
+            sqlConnection.Open();
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0]);
+            }
+            sqlConnection.Close();
+        }
+    }
+}
