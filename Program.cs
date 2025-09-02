9b0c3bba061c6b4a5b0254635ed800eef416708c
Mena Erian <mena3koko2karas1@gmail.com>
2025-09-02 14:20:07 +0300

    Connection with DB

--- Program.cs
+++ Program.cs
@@ -1,25 +1,28 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            SqlConnection sqlConnection = new SqlConnection(
-                "Server=.;Database=ITIA;integrated security=true;trustservercertificate=true;"
-                );
+            var connString = new ConfigurationBuilder().AddJsonFile("appsetting.json").Build().GetSection("connection1").Value;
+            SqlConnection sqlConnection = new SqlConnection(connString);
             SqlCommand sqlCommand = new SqlCommand("select * from Department;", sqlConnection);
-            
+
             sqlCommand.CommandType = System.Data.CommandType.Text;
-            sqlConnection.Open();
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+                sqlConnection.Open();
 
             SqlDataReader reader = sqlCommand.ExecuteReader();
 
             while (reader.Read())
             {
-                Console.WriteLine(reader[0]);
+                $"{reader[0]} :: {reader["Dept_Name"]} :: {reader["Dept_Manager"] ?? "NOT HAVE MANAGER"} ".Print();
             }
             sqlConnection.Close();
+
+            Console.ReadLine();
         }
     }
 }
